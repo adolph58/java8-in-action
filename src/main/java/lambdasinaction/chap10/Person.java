d433efc237dabd33a07a8e532d67a413f5bff842
arte <07761221>
2019-06-15 10:59:00 +0800

    10到14章

---
+++ src/main/java/lambdasinaction/chap10/Person.java
@@ -0,0 +1,12 @@
+package lambdasinaction.chap10;
+
+import java.util.Optional;
+
+public class Person {
+
+    private Optional<Car> car;
+
+    public Optional<Car> getCar() {
+        return car;
+    }
+}
